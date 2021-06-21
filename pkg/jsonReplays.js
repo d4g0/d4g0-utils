735e1001bab961b57f8167c66efd18ff930245db
d4g0 <tocarralero@gmail.com>
2021-06-21 05:28:23 -0400

    ok support

--- pkg/jsonReplays.js
+++ pkg/jsonReplays.js
@@ -1,12 +1,31 @@
 /**
  * Create and returns a  custom `replay`
- * object.
+ * object like the folowing.
+ * Adiccionally adds a `ok` truthy prop if result is `'OK'`
+ * for shorter positive comprobation
  * 
- * @param {object || string}  error
+ * ```js 
+ * {
+ *  error,     // prefered Error instance
+ *  result,    // status string: 'OK' or 'FAIL'
+ *  payload    // anything that must be returned
+ * }
+ * ```
+ * @param {object} error
  * @param {string} result
  * @param {object} payload
  */
-const newReplay = (error = {}, result = "", payload = {}) => {
-    return { error, result, payload }
+const newReplay = (error = {}, result = "OK" || 'FAIL', payload = {}) => {
+    const replay = {
+        error,
+        payload,
+        result
+    }
+
+    if (result == 'OK') {
+        replay.ok = true;
+    }
+
+    return replay
 };
-module.exports = {newReplay}
+module.exports = { newReplay }
--- test.js
+++ test.js
@@ -1,3 +1,17 @@
 const { newReplay } = require('./index.js');
+const { match, ok } = require('assert');
 
-console.log(newReplay(null, "OK", { thing: 'yupi' }))
+
+
+const rep = newReplay(null, 'OK', null);
+const rep1 = newReplay(
+    new Error('Margarita out of mint bep bep bep'),
+    'FAIL',
+    null
+)
+
+
+console.log({
+    rep,
+    rep1
+})
